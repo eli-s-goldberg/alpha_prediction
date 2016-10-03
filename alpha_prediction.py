3b0a5921a8070867a3a0395911524938a1f1acb3
PandaStabber <elisgoldberg@gmail.com>
2016-10-03 13:21:27 -0500

    ready for 100 runs

---
+++ alpha_prediction.py
@@ -0,0 +1,242 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import compress
+from sklearn import metrics, grid_search, cross_validation
+from sklearn.feature_selection import RFECV
+from sklearn.ensemble import GradientBoostingRegressor
+
+from helper_functions import *
+
+DATABASE_PATH = os.path.join('alpha_database.csv')
+
+GBR_INITIAL_PARAMS_ = {'learning_rate': .1,
+                       'max_depth': 3,
+                       'min_samples_leaf': 5,
+                       'max_features': 'auto',
+                       'loss': 'lad',
+                       'subsample':.5,
+                       'n_estimators': 5000}
+
+GBR_PARAMETER_GRID_ = {'learning_rate': [0.3, 0.1],
+                       'max_depth': [5],
+                       'min_samples_leaf': [2,15,30],
+                       'max_features': ['auto','sqrt', 'log2'],
+                       'loss': ['ls', 'lad','huber','quantile'],
+                       'n_estimators': [500]}
+
+
+ONE_HOT_REFORM_CATEGORIES_ = ['nmId', 'shape', 'nomLayer',
+                              'dissNomType', 'saltType', 'prepMethod']
+
+
+# redefine the GBC so we can do RFECV
+class GradientBoostingRegressorWithCoef(GradientBoostingRegressor):
+
+    def fit(self, *args, **kwargs):
+        super(GradientBoostingRegressorWithCoef, self).fit(*args, **kwargs)
+        self.coef_ = self.feature_importances_
+
+
+def main(
+        iterations=1,
+        output_dir='output',
+        one_hot_reform=True,
+        rfecv_eval=True,
+        deterministic=False,
+        grid_search_eval=True,
+        shuffle_holdout=True,
+        plot_rfecv_gridscore=True,
+        optimum_gbr_estimate = True,
+        max_gbr_iterations = 5000,
+        plot_all_gridscores=True,
+        holdout_size=0.15,
+        crossfolds=5,
+        one_hot_reform_categories=ONE_HOT_REFORM_CATEGORIES_,
+        database_path=DATABASE_PATH,
+        target_data_column_name='depAttEff',
+        gbr_parameter_grid_=GBR_PARAMETER_GRID_,
+        gbr_initial_params=GBR_INITIAL_PARAMS_):
+
+    # input database
+    database_basename = os.path.basename(DATABASE_PATH)
+
+    # output directory
+    output_dir = os.path.join(output_dir, 'regressor')
+    make_dirs(output_dir)
+
+    # initialize predicted and holdout tracking
+    rfecv_y_predicted_track = []
+    rfecv_y_holdout_track = []
+
+    # initialize score tracking
+    score_track_mae = []
+    score_track_r2 = []
+    rfecv_gridscore_track = []
+
+    # initialize feature tracking and ranking
+    feature_track = []
+    feature_rank = []
+
+    training_data = pd.read_csv(database_path)
+    target_data = training_data[target_data_column_name]
+
+    # make sure to drop the target data from the training data
+    training_data = training_data.drop([target_data_column_name], 1)
+
+    # initialize the regressor with initial params
+    clf = GradientBoostingRegressorWithCoef(**gbr_initial_params)
+
+    if one_hot_reform:
+        training_data, _, _ = one_hot_dataframe(
+            training_data, one_hot_reform_categories, replace=True)
+
+    for run in xrange(iterations):
+        print "run: ", run+1
+        y_all = np.array(target_data)
+        x_all = training_data.as_matrix()
+
+        if shuffle_holdout:
+            random_state = _SEED if deterministic else None
+            sss = cross_validation.ShuffleSplit(len(y_all),
+                                                n_iter=1,
+                                                test_size=holdout_size,
+                                                random_state=random_state)
+
+            for train_index, test_index in sss:
+                x_train, x_holdout = x_all[train_index], x_all[test_index]
+                y_train, y_holdout = y_all[train_index], y_all[test_index]
+
+        '''The logic is to optimize the parameters for all the features before
+		RFECV'''
+        if grid_search_eval:
+            if optimum_gbr_estimate:
+                # initial params for optimum finding
+
+                # determine minimum number of estimators with least overfitting
+
+                opt_gbr = np.arange(max_gbr_iterations) + 0
+                test_score = heldout_score(clf, x_train, y_train, max_gbr_iterations)
+                test_score /= test_score[0]
+                test_best_iter = opt_gbr[np.argmin(test_score)]
+                print test_best_iter
+
+                # triple the optimum number of estimators.
+                gbr_parameter_grid_['n_estimators'] = [test_best_iter]
+
+                # then implement grid search alg.
+                grid_searcher = grid_search.GridSearchCV(estimator=clf,
+                                                         cv=crossfolds,
+                                                         param_grid=gbr_parameter_grid_,
+                                                         n_jobs=-1)
+
+                # call the grid search fit using the data
+                grid_searcher.fit(x_train, y_train)
+
+                # store and print the best parameters
+                best_params = grid_searcher.best_params_
+
+        else:
+            ''' The logic is that if we don't do grid search, use initial
+                    params as 'best' '''
+            best_params = gbr_initial_params
+
+        # re-initialize and fit with the "best params"
+        clf = GradientBoostingRegressorWithCoef(**best_params)
+        clf.fit(x_train, y_train)
+
+        if rfecv_eval:
+            rfecv = RFECV(
+                estimator=clf,
+                step=1,
+                cv=crossfolds,
+                scoring='mean_absolute_error')
+
+            # perform rfecv fitting
+            rfecv.fit(x_train, y_train)
+
+            # track predicted y values
+            rfecv_y_predicted = rfecv.predict(x_holdout)
+            rfecv_y_predicted_track.append(rfecv_y_predicted)
+
+            # track truth y_holdout values
+            rfecv_y_holdout_track.append(y_holdout)
+
+            # track grid score rankings
+            rfecv_gridscore_track.append(rfecv.grid_scores_)
+
+            # track MAE performance of estimtor to predict holdout
+            score_track_mae.append(metrics.mean_absolute_error(
+                rfecv_y_predicted, y_holdout))
+
+            # track overall r2 performance to predict holdout
+            score_track_r2.append(metrics.r2_score(
+                rfecv_y_predicted, y_holdout))
+
+            # create array of feature ranks (contains all featuers)
+            feature_rank.append(rfecv.ranking_)
+            feat_names = np.array(list(training_data), copy=True)
+
+            # create array of only selected features
+            rfecv_bool = np.array(rfecv.support_, copy=True)
+            sel_feat = list(compress(feat_names, rfecv_bool))
+            feature_track.append(sel_feat)
+
+        if plot_rfecv_gridscore and rfecv_eval:
+            plt.plot(rfecv_y_predicted, y_holdout, '+')
+            plt.plot(y_holdout, y_holdout, 'r-')
+            plt.show()
+
+            plt.xlabel("Number of features selected")
+            plt.ylabel("Cross validation score (MAE)")
+            plt.plot(range(1, len(rfecv.grid_scores_) + 1),
+                     rfecv.grid_scores_)
+            plt.show()
+
+    # Output used to plot the rank of each feature relatively. 
+    feature_rank_df = pd.DataFrame(feature_rank)
+    feature_rank_df.columns = feat_names 
+    feature_rank_df = feature_rank_df.transpose()
+    feature_rank_df.to_csv('feature_rank_df.csv')
+
+    # Output used to plot only the best features
+    feature_track = pd.DataFrame(feature_track)
+    feature_track = feature_track.transpose()
+    feature_track.to_csv('feature_track.csv')
+
+    # overall r2 value for all runs
+    overall_r2 = metrics.r2_score(
+        np.array(rfecv_y_predicted_track).ravel(order='C'), np.array(
+            rfecv_y_holdout_track).ravel(order='C'))
+
+    # Output to plot the predicted y values 
+    rfecv_y_predicted_track = pd.DataFrame(rfecv_y_predicted_track).transpose()
+    rfecv_y_predicted_track.to_csv('rfecv_y_predicted_track.csv')
+
+    # Output to plot the holdout y values (truth)
+    rfecv_y_holdout_track = pd.DataFrame(rfecv_y_holdout_track).transpose()
+    rfecv_y_holdout_track.to_csv('rfecv_y_holdout_track.csv')
+
+    # Output used to plot the optimum model MAE 
+    score_track_mae = pd.DataFrame(score_track_mae).transpose()
+    score_track_mae.to_csv('score_track_mae.csv')
+    print score_track_mae
+
+    # Output used to plot the optimum model r2 
+    score_track_r2 = pd.DataFrame(score_track_r2).transpose()
+    score_track_r2.to_csv('score_track_r2.csv')
+
+    # transpose dataframe for ease of viewing and plotting
+    rfecv_gridscore_track = pd.DataFrame(rfecv_gridscore_track)
+    rfecv_gridscore_track = rfecv_gridscore_track.transpose()
+    rfecv_gridscore_track.to_csv('rfecv_gridscore_track.csv')
+
+    if plot_all_gridscores:
+        rfecv_gridscore_track.plot(kind='line')
+        plt.show()
+
+
+if __name__=="__main__":
+    main(iterations=100, plot_rfecv_gridscore=False, plot_all_gridscores=True)
+
---
+++ helper_functions.py
@@ -0,0 +1,61 @@
+import os
+import pandas as pd
+import errno
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import metrics, grid_search
+from sklearn.metrics import classification_report
+from sklearn import cross_validation
+import sklearn.metrics
+from sklearn.feature_selection import RFECV
+from sklearn.ensemble import GradientBoostingRegressor
+
+
+def one_hot_dataframe(data, cols, replace=False):
+    """
+    Do hot encoding of categorical columns in a pandas DataFrame.
+    See:
+    http://scikit-learn.org/dev/modules/generated/sklearn.preprocessing.OneHotEncoder.html#sklearn.preprocessing
+    .OneHotEncoder
+    http://scikit-learn.org/dev/modules/generated/sklearn.feature_extraction.DictVectorizer.html
+    https://gist.github.com/kljensen/5452382
+    Takes a dataframe and a list of columns that need to be encoded.
+        Returns a 3-tuple comprising the data, the vectorized data,
+        and the fitted vectorizor.
+    """
+    from sklearn.feature_extraction import DictVectorizer
+    vec = DictVectorizer()
+    mkdict = lambda row: dict((col, row[col]) for col in cols)
+    vec_data = pd.DataFrame(vec.fit_transform(
+        data[cols].apply(mkdict, axis=1)).toarray())
+    vec_data.columns = vec.get_feature_names()
+    vec_data.index = data.index
+    if replace is True:
+        data = data.drop(cols, axis=1)
+        data = data.join(vec_data)
+
+    return (data, vec_data, vec)
+
+
+def make_dirs(path):
+    """Recursively make directories, ignoring when they already exist.
+    See: http://stackoverflow.com/a/600612/1275412
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno != errno.EEXIST or not os.path.isdir(path):
+            raise
+
+
+
+def heldout_score(clf, X_test, y_test, n_estimators):
+    """compute deviance scores on ``X_test`` and ``y_test``.
+    See: http://scikit-learn.org/stable/auto_examples/ensemble/plot_gradient_boosting_oob.html"""
+    clf.fit(X_test, y_test)
+    score = np.zeros((n_estimators,), dtype=np.float64)
+    for i, y_pred in enumerate(clf.staged_predict(X_test)):
+        # clf.loss_ assumes that y_test[i] in {0, 1}
+        score[i] = clf.loss_(y_test, y_pred)
+    return score
+
